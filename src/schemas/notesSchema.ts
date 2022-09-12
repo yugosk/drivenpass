8d9a65dac81c68445dd95d713630c769d3d2cd2e
Yugo <yugosakamoto0717@gmail.com>
2022-09-12 16:32:37 -0300

    [fix] Corrects notesSchema Joi ObjectSchema

--- src/schemas/notesSchema.ts
+++ src/schemas/notesSchema.ts
@@ -2,7 +2,7 @@ import Joi, { ObjectSchema } from "joi";
 
 const noteSchema: ObjectSchema = Joi.object({
   title: Joi.string().max(50).required(),
-  description: Joi.string().max(1000).required,
+  description: Joi.string().max(1000).required(),
 });
 
 export default noteSchema;
