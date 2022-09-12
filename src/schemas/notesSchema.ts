9d54dddc16b74821f7e9d20eff816de1be8b0109
Yugo <yugosakamoto0717@gmail.com>
2022-09-12 16:31:33 -0300

    [fix] Adds some types

--- src/schemas/notesSchema.ts
+++ src/schemas/notesSchema.ts
@@ -1,6 +1,6 @@
 import Joi, { ObjectSchema } from "joi";
 
-const noteSchema = Joi.object({
+const noteSchema: ObjectSchema = Joi.object({
   title: Joi.string().max(50).required(),
   description: Joi.string().max(1000).required,
 });
